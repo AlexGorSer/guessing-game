8ace7bd0b2ecb58f13230dde0daadb17ec50ac02
AlexGorSer <taskekaminari@gmail.com>
2022-08-15 20:49:09 +0200

    add: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.min;
+        this.max;
+    };
+    averNumber() {
+        let num = Math.ceil((this.max + this.min) / 2)
+        return num;
+    };
     setRange(min, max) {
-
-    }
-
+        this.min = min;
+        this.max = max;
+    };
     guess() {
-
-    }
-
+        return this.averNumber();
+    };
     lower() {
-
-    }
-
+        this.max = this.averNumber();
+    };
     greater() {
-
-    }
+        this.min = this.averNumber();
+    };
 }
 
 module.exports = GuessingGame;
